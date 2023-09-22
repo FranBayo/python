0d33e3ef7fbdc5075478f9f002c74e4bbdf74f76
FranBayo <145747020+FranBayo@users.noreply.github.com>
2023-09-22 11:08:28 +0200

    Add files via upload

---
+++ HolaMundo.py
@@ -0,0 +1,31 @@
+#yume no koto de ishasi sawa de
+print("Hola Mundo")
+print("Me llamo Celia") #sólo imprime texto entre comillas, no texto a secas (que son variables)
+#nuestra primera variable
+celia = 7
+print(celia)    #imprime variable numérica
+print("print 7+8:")
+print(celia + 8)    #las variables se pueden sumar
+#varible con texto: string
+nombre = "Celia"
+print(nombre)
+print(nombre + "@gmail.com")
+print("suma nombre+número")
+#print(nombre + celia)  no se puede
+print(nombre + str(celia))  #así si
+print("print('Celia'+'7')")
+print("print(\"Celia\"+\"7\")")
+print("Escribo 20 veces cu")
+print("cucu"*10)
+
+print()
+
+print("\n\\Hacemos \t\"operaciones\"\t matemáticas\\")
+num1=2
+num2=8
+suma=num1+num2
+resta=num1-num2
+multp=num1*num2
+divis=num1/num2
+print("suma = "+str(suma) + ";resta = " +str(resta) +";multiplicación = "
+      +str(multp)+";división ="+str(divis))
