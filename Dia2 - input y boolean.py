d04eb4a07ffdc65a89304da277e030dc02dfa7f9
FranBayo <145747020+FranBayo@users.noreply.github.com>
2023-09-26 08:05:09 +0200

    Add files via upload
    input y booleanos (que te metan datos y hacer cosas con ellos)

---
+++ Dia2 - input y boolean.py
@@ -0,0 +1,29 @@
+#El imput recoge el dato y lo guarda en el nombre (toma un print de regalo: print()
+#   nombre = input("pon tu nombre ")
+#   print(nombre)
+#Hay dos iguales en programación que son diferentes:
+# = (asigna lo de la izquierda a lo de la derecha), 
+# y == (compara que izquierda y derecha sean iguales, devuelve true o false)
+#ejercicio1: si es mayor de edad = true
+#   edad = input("Introduce una edad: ")
+#print(type(edad)) ; sirve para ver la categoría de la variable.
+#Lo que entra con input SIEMRPE es string, tiene que cambiarse a mano,
+#también se puede cambiar mientras entra, int(input(XXXXX))
+#   print("es mayor de edad?")
+#   print(int(edad) >= 18)
+#ejercicio2: si es fin de semana = true
+#   dia = input("Introduce un dia: ")
+#   print("es fin de semana?")
+#   print(dia == "Sabado" or dia == "Domingo")
+#ejercicio3: si es un número positivo = true
+#   mas = int(input("Introduce un numero: "))
+#   print("es positivo?")
+#   print(mas >= 0)
+#ejercicio4: si es vocal = true
+#   letra = input("Introduce una letra: ")
+#   print("es una vocal?")
+#   print(letra == "A" or letra == "E" or letra == "I" or letra == "O" or letra == "U")
+#ejercicio5: Nota aprobada = true (examen de 0 a 10)
+#   nota = int(input("Introduce una nota: "))
+#   print("está aprobada?")
+#   print(nota > 4 and nota <= 10)
